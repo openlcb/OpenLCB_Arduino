c4d51dd402d86152e0acd2391141da83490a2335
Bob Jacobsen <jacobsen@mac.com>
2010-06-27 18:20:36 +0000

    fix hex format
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/C@428 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

--- scaffold/logging.h
+++ scaffold/logging.h
@@ -4,6 +4,6 @@
 #ifndef lognl
 #include <stdio.h>
 #define logstr(...) printf(__VA_ARGS__)
-#define loghex(...) printf("%h",__VA_ARGS__)
+#define loghex(...) printf("%x",__VA_ARGS__)
 #define lognl() printf("\n")
 #endif  // logln
