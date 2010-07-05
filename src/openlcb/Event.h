8b67805c69ef9bb19264bb47bfff6a59c47df693
Bob Jacobsen <jacobsen@mac.com>
2010-07-05 03:49:19 +0000

    split off .cpp, add ctor for state
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/C@543 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

--- src/openlcb/Event.h
+++ src/openlcb/Event.h
@@ -6,37 +6,29 @@
 class Event : public EventID {
   public: 
 
-  Event() {
-  }
+  Event();
+
+  Event(bool produce, bool consume);
   
-  Event(uint8_t b0, uint8_t b1, uint8_t b2, uint8_t b3, uint8_t b4, uint8_t b5, uint8_t b6, uint8_t b7) 
-      : EventID(b0, b1, b2, b3, b4, b5, b6, b7), flags(0){
-  }
+  Event(uint8_t b0, uint8_t b1, uint8_t b2, uint8_t b3, uint8_t b4, uint8_t b5, uint8_t b6, uint8_t b7); 
   
-  bool equals(Event* n) {
-    return  EventID::equals(n);  // just check ID for now
-  }
+  bool equals(Event* n);
   
   /**
    * Check to see if this object is equal
    * to any in an array of Events
    */
-  Event* findEventInArray(Event* array, int len) {
-      for (int i = 0; i<len; i++) {
-          if (equals(array+i)) return array+i;
-      }
-      return 0;
-  }
+  Event* findEventInArray(Event* array, int len);
   
-  int findIndexInArray(Event* array, int len) {
-      for (int i = 0; i<len; i++) {
-          if (equals(array+i)) return i;
-      }
-      return -1;
-  }
-
+  int findIndexInArray(Event* array, int len, int start);
+  
   // bit mask local flags
   int flags;
+  
+  // Mark entry as consumer
+  static const int CAN_CONSUME_FLAG = 0x20;
+  // Mark entry as producer
+  static const int CAN_PRODUCE_FLAG = 0x40;
 };
 
 #endif
