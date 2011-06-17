1b9d191f5fa0c3c9a3632b1eba4d7936ba1bd1be
Don Goodman-Wilson <don@screenhero.com>
2011-06-17 21:02:14 +0000

    Another adjustment for the sake of the aforementioned memory leak.
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/C@1301 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

--- libraries/OpenLCB/OLCB_Handler.cpp
+++ libraries/OpenLCB/OLCB_Handler.cpp
@@ -3,11 +3,12 @@
 void OLCB_Handler::setNID(OLCB_NodeID *newNID)
 {
   //Only allocate memory for a new NID the first time this gets called!
-  if(NID == _link->getNodeID()) //don't want to over-write the link's ID!
+  if(!NID || (NID == _link->getNodeID())) //don't want to over-write the link's ID!
     NID = (OLCB_NodeID*)malloc(sizeof(OLCB_NodeID));
   memcpy(NID,newNID, sizeof(OLCB_NodeID));
 }
 
+//this really should only be called once.
 void OLCB_Handler::setLink(OLCB_Link *newLink)
 {
   _link = newLink;
