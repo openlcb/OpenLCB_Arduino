213829e6ef027cada94f7906712cfa0da7122963
Bob Jacobsen <jacobsen@mac.com>
2012-02-14 15:16:49 +0000

    Arduino 1.0
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/C@1816 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

--- arduino.mk
+++ arduino.mk
@@ -5,4 +5,4 @@ ARDUINO_ROOT := /Applications/Arduino.app/Contents/Resources/Java/
 
 CC_OPTIONS_ARDUINO := -Os -w -fno-exceptions -ffunction-sections -fdata-sections -std=gnu99 -mmcu=atmega168 -DF_CPU=16000000L
 
-INCLUDE_ARDUINO := -I${ARDUINO_ROOT}hardware/arduino/cores/arduino 
+INCLUDE_ARDUINO := -I${ARDUINO_ROOT}hardware/arduino/cores/arduino -I${ARDUINO_ROOT}hardware/arduino/variants/standard
--- standard.mk
+++ standard.mk
@@ -9,7 +9,8 @@ INCLUDE_ARDUINO := ${INCLUDE_ARDUINO} -I${ARDUINO_ROOT}libraries/Servo
 # note that this in meant to be included one level down, where PWD refers to a specific examples/sketch
 
 # local libraries
-INCLUDE_OPTIONS := -I${PWD}/libraries/OlcbArduinoCAN 
+INCLUDE_OPTIONS :=                    -I${PWD}/libraries/ButtonLED 
+INCLUDE_OPTIONS := ${INCLUDE_OPTIONS} -I${PWD}/libraries/OlcbArduinoCAN 
 INCLUDE_OPTIONS := ${INCLUDE_OPTIONS} -I${PWD}/libraries/OlcbCommonCAN 
 INCLUDE_OPTIONS := ${INCLUDE_OPTIONS} -I${PWD}/libraries/CAN 
 INCLUDE_OPTIONS := ${INCLUDE_OPTIONS} -I${PWD}/libraries/LCmini
@@ -26,14 +27,32 @@ INCLUDE_OPTIONS := ${INCLUDE_OPTIONS} -I${ARDUINO_ROOT}libraries/Ethernet
 
 TMPDIR := $(shell mktemp -d /tmp/Arduino.XXXXXXXX)
 
-# assumes only one PDE file
+io: TARGET = at90can128
+io: MCU_TARGET = at90can128
+io: CFLAGS += '-DMAX_TIME_COUNT=F_CPU>>4' '-DNUM_LED_FLASHES=3'
+#  -DBAUD_RATE=57600
+io: AVR_FREQ = 16000000L
+#io: LDSECTION  = --section-start=.text=0xF000
+io: LDSECTION  = --section-start=.text=0x1E000
+io: ${INO}${PDE}_io.hex
+
+%.hex: %.elf
+	$(OBJCOPY) -j .text -j .data -O ihex $< $@
+
+# assumes only one PDE or INO file
 PDE := $(foreach V,$(wildcard *.pde),$(V:.pde=))
+INO := $(foreach V,$(wildcard *.ino),$(V:.ino=))
 
 # copy header files, if any, to temp build directory
 $(foreach V,$(wildcard *.h),${TMPDIR}/$(V)):
 	cp *.h ${TMPDIR}/
 
 all: $(foreach V,$(wildcard *.h),${TMPDIR}/$(V))
-	cp ${PDE}.pde ${TMPDIR}/${PDE}.cpp
-	cd ${TMPDIR}; ${ARDUINO_ROOT}hardware/tools/avr/bin/avr-g++ -c -g  ${CC_OPTIONS_ARDUINO} ${INCLUDE_ARDUINO} ${INCLUDE_OPTIONS} *.cpp -o /dev/null
-	rm -r ${TMPDIR}
+	-cp ${INO}.ino ${TMPDIR}/${INO}.cpp
+	-cp ${PDE}.pde ${TMPDIR}/${PDE}.cpp
+	cd ${TMPDIR}; ${ARDUINO_ROOT}hardware/tools/avr/bin/avr-g++ -c -g  ${CC_OPTIONS_ARDUINO} ${INCLUDE_ARDUINO} ${INCLUDE_OPTIONS} *.cpp 
+
+#	rm -r ${TMPDIR}
+
+
+
