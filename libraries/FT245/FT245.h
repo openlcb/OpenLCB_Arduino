a27254c5354e60453859b15cc212a7fc551227be
Alex Shepherd <kiwi64ajs@gmail.com>
2012-09-01 07:57:13 +0000

    Added copywrite info and reference to other source
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/C@2670 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

--- libraries/FT245/FT245.h
+++ libraries/FT245/FT245.h
@@ -1,6 +1,28 @@
-/*
-  FT245.h - FTDI library
-*/
+/****************************************************************************
+ *  FT245.h - FTDI library
+ *  
+ * 	Copyright (C) 2012 Alex Shepherd
+ * 
+ * 	This library is free software; you can redistribute it and/or
+ * 	modify it under the terms of the GNU Lesser General Public
+ * 	License as published by the Free Software Foundation; either
+ * 	version 2.1 of the License, or (at your option) any later version.
+ * 
+ * 	This library is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * 	Lesser General Public License for more details.
+ * 
+ * 	You should have received a copy of the GNU Lesser General Public
+ * 	License along with this library; if not, write to the Free Software
+ * 	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  Inspired by the code found here: 
+	// Web Link		: http://www.codeforge.com/read/156091/ft245_io.c__html
+	// Title		: FTDI FT245 USB Inferface Driver Library
+	// Author		: Pascal Stang - Copyright (C) 2004
+	// Created		: 2004.02.10
+ */
 
 #ifndef FT245_h
 #define FT245_h
