74a23720f1be8f9f403f94a24b6f74098ccbae41
Bob Jacobsen <jacobsen@mac.com>
2012-09-02 21:03:27 +0000

    and remove again, now that its in the repository
    git-svn-id: svn+ssh://svn.code.sf.net/p/openlcb/svn/trunk/prototypes/C@2688 87536cb5-86f1-4cdc-8fc6-3f0f71bcd267

--- libraries/OlcbCommonCAN/decompress.h
+++
@@ -1,9 +0,0 @@
-//
-// "Memory-efficient decompression for embedded computers"
-//
-// See http://excamera.com/sphinx/article-compression.html 
-// for more info
-//
-//#include <sys/types.h>
-#include <WProgram.h>
-static void decompress(byte *dst, byte *src);
